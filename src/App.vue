21e88a7b62d0c4e3ae23b25505d06f8b6b99d6f1
Alessio Napoli <alessionapoli99@icloud.com>
2022-02-18 14:56:48 +0100

    End Exercise

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HeroImg></HeroImg>
+    <TitleVue></TitleVue>
+  </div>
+</template>
+
+<script>
+  import TitleVue from "./components/Title.vue";
+  import HeroImg from "./components/Hero.vue";
+
+export default {
+  name: "App",
+  components: {
+    TitleVue,
+    HeroImg
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
